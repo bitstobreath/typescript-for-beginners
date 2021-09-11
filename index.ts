80063315389b6b4710bfe31c6b97bae9ef973177
Austin H <90514024+bitstobreath@users.noreply.github.com>
2021-09-11 10:21:39 -0400

    Video Edit

--- index.ts
+++ index.ts
@@ -1,16 +1,28 @@
+type Eye = {
+  shape: string;
+  color: string;
+}
+
 type User = {
-    name: string;
-    age: number;
-  };
-  
-  function isAdult(user: User): boolean {
-    return user.age >= 18;
+  name: string;
+  age: number;
+  living: boolean;
+  eye: Eye;
+};
+
+function isAdult(user: User): boolean {
+  return user.age >= 18;
+}
+
+const justine: User = {
+  name: 'Justine',
+  age: 17,
+  living: true,
+  eye: {
+    shape: 'round',
+    color: 'brown'
   }
-  
-  const justine: User = {
-    name: 'Justine',
-    age: 23,
-  };
-  
-  const isJustineAnAdult: boolean = isAdult(justine);
-  console.log("Justine is an adult" + (isJustineAnAdult ? " yes"  : " no"))
+};
+
+const isJustineAnAdult: boolean = isAdult(justine);
+console.log("Justine is an adult" + (isJustineAnAdult ? " yes"  : " no"))
